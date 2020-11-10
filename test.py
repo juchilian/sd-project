d4576a331736607d529d5005cee6fb2d260f778d
juchilian <juchilian@outlook.jp>
2020-11-10 16:32:53 +0900

    [add] race1_class.py クラス構造に書き換え class GameとPlayer
    起動画面まで行くが車のスピードに関するいくつかのバグ発生

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import pygame
+
 class Constants:
     #色の定義
     WHITE  = (255,255,255)
@@ -22,9 +24,22 @@ class Constants:
     ]
     CLEN = len(DATA_LR)  #左右のデータの要素数を代入した定数
 
+class Game:
+    def __init__(self):
+        self.load_image()
+    
+    def load_image(self): #画像の読み込み
+        self.img_bg = pygame.image.load("image_pr/bg.png").convert()  #背景(空と地面の絵)
+    
+    def run(self):
+        pygame.init() #pygameモジュールの初期化                                                
+        pygame.display.set_caption("Pygame Racer") #ウインドウに表示するタイトルを指定
+        screen = pygame.display.set_mode((800,600)) #描画面を初期化
+        print(self.img_bg)
+
 
 
 if __name__ == '__main__':
-    const = Constants()
-    print(const.WHITE)
-    print(const.CLEN)
+    g = Game()
+    g.run()
+    
