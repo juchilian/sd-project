dbfbbcfd9cc27e313864cd738a2dd8725373d137
kumityo <takafumi-fujii0826@keio.jp>
2020-12-22 12:19:13 +0900

    今日の成果2

--- pulse.py
+++ pulse.py
@@ -15,7 +15,6 @@ class Pulse:
 
     #心拍数を計測してself.dataに格納
     def pulse_socket(self):
-<<<<<<< HEAD
         #試したい場合のコード
         self.data = random.randint(200,300)
         print(self.data)
@@ -48,39 +47,3 @@ class Pulse:
 
 pulse = Pulse()
 pulse.pulse_socket()
-=======
-        use_pulse = False
-        if use_pulse:
-            #心拍が取れている場合のコード    
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                # コネクションを試みる
-                s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)# ここで毎回切断
-                s.bind(('192.168.100.113', self.PORT))
-                s.listen()
-                print("Listen Now...")
-                (connection, client) = s.accept()
-
-                # try:
-                print('Client connected', client)
-                #receive data from server
-                #データがリアルタイムに変化する
-                try:
-                    self.data = connection.recv(self.BUFFER_SIZE)
-                #このデータが心拍データ。関数の返り値にできれば藤井のやつと簡単に組み合わせられる！！
-                # connection.send(data)
-                    return self.data
-                
-                finally:
-                    connection.close()
-        else:
-            #試したい場合のコード
-            self.data = random.randint(0,200)
-            print(self.data)
-            return self.data
-
-
-
-
-# pulse = Pulse()
-# pulse.pulse_socket()
->>>>>>> 194ff5a0922807c1d6abe8c43783fd354902fb71
