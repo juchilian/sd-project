998c98a6b8b0937f8a169e7fbc01a0c19fbcf58e
kumityo <takafumi-fujii0826@keio.jp>
2020-12-22 11:32:58 +0900

    顔認識選択画面を追加

--- pulse.py
+++ pulse.py
@@ -21,13 +21,13 @@ class Pulse:
         return self.data
 
 
-        #心拍が取れている場合のコード    
-<<<<<<< HEAD
+        # #心拍が取れている場合のコード    
         # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         #     # コネクションを試みる
         #     s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)# ここで毎回切断
-        #     s.bind(('192.168.100.112', self.PORT))
+        #     s.bind(('192.168.100.113', self.PORT))
         #     s.listen()
+        #     print("Listen Now...")
         #     (connection, client) = s.accept()
 
         #     # try:
@@ -39,28 +39,9 @@ class Pulse:
         #     #このデータが心拍データ。関数の返り値にできれば藤井のやつと簡単に組み合わせられる！！
         #     # connection.send(data)
         #         return self.data
-=======
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            # コネクションを試みる
-            s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)# ここで毎回切断
-            s.bind(('192.168.100.113', self.PORT))
-            s.listen()
-            print("Listen Now...")
-            (connection, client) = s.accept()
-
-            # try:
-            print('Client connected', client)
-            #receive data from server
-            #データがリアルタイムに変化する
-            try:
-                self.data = connection.recv(self.BUFFER_SIZE)
-            #このデータが心拍データ。関数の返り値にできれば藤井のやつと簡単に組み合わせられる！！
-            # connection.send(data)
-                return self.data
->>>>>>> 0aef2cb66eef7539aeb65950d983198cbb46ad71
             
-        #     finally:
-        #         connection.close()
+        # #     finally:
+        # #         connection.close()
 
 
 
