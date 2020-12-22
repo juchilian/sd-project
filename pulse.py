58340d278482ded5c74bcfdf7c26cb0cad4cd5ee
bridge-of-science-and-humanities <61342410+bridge-of-science-and-humanities@users.noreply.github.com>
2020-12-22 10:07:52 +0900

    KCF

--- pulse.py
+++ pulse.py
@@ -16,46 +16,33 @@ class Pulse:
     #心拍数を計測してself.dataに格納
     def pulse_socket(self):
         #試したい場合のコード
-        self.data = random.randint(0,200)
-        print(self.data)
-        return self.data
+        # self.data = random.randint(0,200)
+        # print(self.data)
+        # return self.data
 
 
-        # 心拍が取れている場合のコード    
-        # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        #     # コネクションを試みる
-        #     s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)# ここで毎回切断
-        #     s.bind(('192.168.11.7', self.PORT))
-        #     s.listen()
-        #     (connection, client) = s.accept()
+        #心拍が取れている場合のコード    
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            # コネクションを試みる
+            s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)# ここで毎回切断
+            s.bind(('192.168.100.112', self.PORT))
+            s.listen()
+            (connection, client) = s.accept()
 
-        #     # try:
-        #     print('Client connected', client)
-        #     #receive data from server
-        #     #データがリアルタイムに変化する
-        #     try:
-        #         self.data = connection.recv(self.BUFFER_SIZE)
-        #     #このデータが心拍データ。関数の返り値にできれば藤井のやつと簡単に組み合わせられる！！
-        #     # connection.send(data)
-        #         return self.data
+            # try:
+            print('Client connected', client)
+            #receive data from server
+            #データがリアルタイムに変化する
+            try:
+                self.data = connection.recv(self.BUFFER_SIZE)
+            #このデータが心拍データ。関数の返り値にできれば藤井のやつと簡単に組み合わせられる！！
+            # connection.send(data)
+                return self.data
             
-        #     finally:
-        #         connection.close()
+            finally:
+                connection.close()
 
 
 
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.bind(('192.168.11.7',50000))
-#     print("clear bind")
-#     s.listen()
-#     print("clear listen")
-#     while True:
-#         (connection, client) = s.accept()
-#         print("clear accept")
-#         try:
-#             print('Client connected', client)
-#             data = connection.recv(1024)
-#             # connection.send(data.upper())
-#             print(data)
-#         finally:
-#             connection.close()
+pulse = Pulse()
+pulse.pulse_socket()
